22c9444a400434c77d136f307c2f06692741935c
Pravinthan Prabagaran <pravinthan.prabagaran@gmail.com>
2020-06-28 01:34:46 -0400

    Added component activation guard for the my-picks route

--- pick-easy/src/app/app-routing.module.ts
+++ pick-easy/src/app/app-routing.module.ts
@@ -6,13 +6,18 @@ import { PageNotFoundComponent } from "./components/pages/page-not-found/page-no
 import { HomeComponent } from "./components/pages/home/home.component";
 import { CreditsComponent } from "./components/pages/credits/credits.component";
 import { MyPicksComponent } from "./components/my-picks/my-picks.component";
+import { AuthenticationGuard } from "./shared/authentication.guard";
 
 const routes: Routes = [
   { path: "", pathMatch: "full", component: HomeComponent },
   { path: "signin", component: SignInComponent },
   { path: "signup", component: SignUpComponent },
   { path: "credits", component: CreditsComponent },
-  { path: "my-picks", component: MyPicksComponent },
+  {
+    path: "my-picks",
+    component: MyPicksComponent,
+    canActivate: [AuthenticationGuard],
+  },
   { path: "**", component: PageNotFoundComponent },
 ];
 
