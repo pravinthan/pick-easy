8803e5485c3f1621fdb4b707eac28d9263f3fc81
Luke Zhang <lukezhang1998@gmail.com>
2020-07-11 16:06:37 -0400

    Added Restaurant Details Component

---
+++ pick-easy/src/app/components/pages/consumer/discover/restaurant-details/restaurant-details.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RestaurantDetailsComponent } from './restaurant-details.component';
+
+describe('RestaurantDetailsComponent', () => {
+  let component: RestaurantDetailsComponent;
+  let fixture: ComponentFixture<RestaurantDetailsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RestaurantDetailsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RestaurantDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
